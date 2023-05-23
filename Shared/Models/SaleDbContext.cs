b8c3a0c8255093c3e69e35af973b4fd30d826d00
Jcervantes03 <chelisguapo@hotmail.com>
2023-05-23 12:22:57 -0600

    first commit

---
+++ Shared/Models/SaleDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class PrestamoDbContext : DbContext
+{
+    public DbSet<Prestamo> Prestamos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+       
+        optionsBuilder.UseSqlServer("Server=localhost;Database=Bd_prestamo;User Id=root;Password=Joseluis03");
+    }
+}
